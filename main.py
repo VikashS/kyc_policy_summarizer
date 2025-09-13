47c83db28e6e7c8b43fb76bab8c4caea92c0da69
Vikash Singh <glovesdown@gmail.com>
2025-09-13 23:48:23 +0200

    first

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
 from dotenv import load_dotenv
 
+load_dotenv()
 
 def main():
     print("Hello from kyc-policy-summarizer!")
+    res=os.environ.get("OPENAI_API_KEY")
+    print(res)
 
 
 if __name__ == "__main__":
